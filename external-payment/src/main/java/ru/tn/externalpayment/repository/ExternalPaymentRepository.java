69354830b68b884408509ad579b72a636e5e2a3f
rnemykin <nemykinroman@gmail.com>
2017-05-04 23:27:59 +0300

    move to pack

--- external-payment/src/main/java/ru/tn/externalpayment/repository/ExternalPaymentRepository.java
+++ external-payment/src/main/java/ru/tn/externalpayment/repository/ExternalPaymentRepository.java
@@ -1,4 +1,4 @@
-package ru.tn.repository;
+package ru.tn.externalpayment.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
