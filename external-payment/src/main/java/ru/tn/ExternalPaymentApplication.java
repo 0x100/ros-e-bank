142e73df237f001089f3e6d1611eb82655163d5e
rnemykin <nemykinroman@gmail.com>
2017-05-04 23:06:58 +0300

    external payment gw publishing

--- external-payment/src/main/java/ru/tn/ExternalPaymentApplication.java
+++ external-payment/src/main/java/ru/tn/ExternalPaymentApplication.java
@@ -2,10 +2,12 @@ package ru.tn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import ru.tn.gateway.publish.annotation.EnableGatewayPublishing;
 import ru.tn.gateway.publish.annotation.GatewayService;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableGatewayPublishing(@GatewayService(path = "/external-payments/**", url = "/external-payments/"))
 public class ExternalPaymentApplication {
 
