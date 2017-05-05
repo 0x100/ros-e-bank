3173699d0e7a2f816b01b043eb261cea464b82af
rnemykin <nemykinroman@gmail.com>
2017-05-05 22:28:04 +0300

    gw publish fix

--- gateway-publisher-spring-boot-starter/src/main/java/ru/tn/gateway/publish/annotation/EnableGatewayPublishing.java
+++ gateway-publisher-spring-boot-starter/src/main/java/ru/tn/gateway/publish/annotation/EnableGatewayPublishing.java
@@ -1,5 +1,8 @@
 package ru.tn.gateway.publish.annotation;
 
+import org.springframework.context.annotation.Import;
+import ru.tn.gateway.publish.config.GatewayPublisherConfiguration;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +10,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Import(GatewayPublisherConfiguration.class)
 public @interface EnableGatewayPublishing {
     GatewayService[] value();
 }
