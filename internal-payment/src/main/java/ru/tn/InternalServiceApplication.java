885b7bdcda0d07ec4950174b3f3b75cfec351d7b
dsnimshchikov <Dmitriy.Snimshchikov@homecredit.ru>
2017-05-11 00:19:06 +0300

    [ #943] Микросервис внутренних переводов

---
+++ internal-payment/src/main/java/ru/tn/InternalServiceApplication.java
@@ -0,0 +1,22 @@
+package ru.tn;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import ru.tn.gateway.publish.annotation.EnableGatewayPublishing;
+import ru.tn.gateway.publish.annotation.GatewayService;
+
+/**
+ * @author dsnimshchikov on 04.05.17.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableGatewayPublishing(@GatewayService(path = "/internal-payments/**", url = "/internal-payments/"))
+public class InternalServiceApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder().sources(InternalServiceApplication.class)
+                .listeners(new ApplicationPidFileWriter())
+                .run(args);
+    }
+}
