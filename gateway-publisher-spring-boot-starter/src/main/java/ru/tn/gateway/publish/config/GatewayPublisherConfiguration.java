55233506b4b335297398714a19d857085361c526
dsnimshchikov <Dmitriy.Snimshchikov@homecredit.ru>
2017-05-22 18:44:31 +0300

    [ #943] static metod return after merge

--- gateway-publisher-spring-boot-starter/src/main/java/ru/tn/gateway/publish/config/GatewayPublisherConfiguration.java
+++ gateway-publisher-spring-boot-starter/src/main/java/ru/tn/gateway/publish/config/GatewayPublisherConfiguration.java
@@ -68,4 +68,8 @@ public class GatewayPublisherConfiguration {
             }
         }
     }
+
+    public static String getGatewayServiceUrlKey(String serviceName) {
+        return GATEWAY_SERVICE_KEY + serviceName + GATEWAY_SERVICE_URL;
+    }
 }
