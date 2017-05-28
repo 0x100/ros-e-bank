08dd6263f1ae5408330c105e2c65b4d198ef13ec
Alexander Shestakov <Aleksandr.Shestakov@homecredit.ru>
2017-05-28 13:35:51 +0300

    rancher: replace echo on stage

--- rancher/jobs/deploy-infrastructure-generic.groovy
+++ rancher/jobs/deploy-infrastructure-generic.groovy
@@ -7,18 +7,18 @@ node(){
     try {
         withCredentials([[$class: "UsernamePasswordMultiBinding", credentialsId: "rancher", usernameVariable: "AKEY", passwordVariable: "SKEY"]]) {
             try {
-                echo "Deploy Application"
-                timeout(time: 300, unit: 'SECONDS') {
-                    sh ". ${env.WORKSPACE}/rancher/vars/main;" +
-                            "rancher-compose " +
-                            "--debug " +
-                            "--url=http://127.0.0.1:8080/ " +
-                            "--access-key=${env.AKEY} " +
-                            "--secret-key=${env.SKEY} " +
-                            "--file=rancher/roles/${env.stack_name}/docker-compose.yml " +
-                            "--rancher-file=rancher/roles/${env.stack_name}/rancher-compose.yml " +
-                            "up -d --pull --upgrade --batch-size '1'"
-                }
+                stage "Deploy Application"
+                    timeout(time: 300, unit: 'SECONDS') {
+                        sh ". ${env.WORKSPACE}/rancher/vars/main;" +
+                                "rancher-compose " +
+                                "--debug " +
+                                "--url=http://127.0.0.1:8080/ " +
+                                "--access-key=${env.AKEY} " +
+                                "--secret-key=${env.SKEY} " +
+                                "--file=rancher/roles/${env.stack_name}/docker-compose.yml " +
+                                "--rancher-file=rancher/roles/${env.stack_name}/rancher-compose.yml " +
+                                "up -d --pull --upgrade --batch-size '1'"
+                    }
 
             } catch (Exception err) {
                 echo "Rolling back"
@@ -33,8 +33,8 @@ node(){
                         "up -d --rollback"
                 throw new Exception("Can't deploy applications. Rolling back..", err)
             }
-            echo "Confirm Upgrade"
-            sh ". ${env.WORKSPACE}/rancher/vars/main;" +
+            stage "Confirm Upgrade"
+                 sh ". ${env.WORKSPACE}/rancher/vars/main;" +
                     "rancher-compose " +
                     "--debug " +
                     "--url=http://127.0.0.1:8080/ " +
