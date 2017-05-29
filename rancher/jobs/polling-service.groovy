e499c46f0f0fbebc16883f48376cf2493e6d9e65
Alexander Shestakov <Aleksandr.Shestakov@homecredit.ru>
2017-05-29 14:44:27 +0300

    git url hardcode

--- rancher/jobs/polling-service.groovy
+++ rancher/jobs/polling-service.groovy
@@ -5,17 +5,19 @@ node('master') {
     m = null
 
     def downstreamJob = 'build-service-pipeline-generic'
+    def git_url = 'ssh://git@rt.reliab.tech:55522/reb/microservices.git'
+    def git_branch = 'master'
 
     try {
         step([$class: 'WsCleanup']) // clean workspace
 
         stage 'Pulling sources'
-            git branch: env.GIT_BRANCH, url: env.GIT_URL
+            git branch: git_branch, url: git_url
 
         stage 'Build & Deploy Application'
             def buildResult = build job: downstreamJob, parameters: [
-                    string(name: 'GIT_BRANCH', value: env.GIT_BRANCH),
-                    string(name: 'GIT_URL', value: env.GIT_URL),
+                    string(name: 'GIT_BRANCH', value: git_branch),
+                    string(name: 'GIT_URL', value: git_url),
                     string(name: 'DOCKER_TAG', value: env.BUILD_ID),
                     string(name: 'app_name', value: app_name)
             ]
