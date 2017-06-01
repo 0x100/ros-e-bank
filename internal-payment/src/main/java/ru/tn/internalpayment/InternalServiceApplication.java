a946801de09d13fa8c3ea22fb5e9ed8d4e49bcb5
Ilya Lysenko <lysenko.ilya@gmail.com>
2017-06-02 00:27:08 +0300

    circuit breaker for call external-payment from InternalService

--- internal-payment/src/main/java/ru/tn/internalpayment/InternalServiceApplication.java
+++ internal-payment/src/main/java/ru/tn/internalpayment/InternalServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import ru.tn.errorhandler.EnableErrorHandle;
 import ru.tn.gateway.publish.annotation.EnableGatewayPublishing;
@@ -18,7 +18,7 @@ import ru.tn.gateway.publish.annotation.GatewayService;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableHystrix
+@EnableCircuitBreaker
 @EnableGatewayPublishing(@GatewayService(path = "/internal-payments/**", url = "/internal-payments/"))
 @EntityScan("ru.tn.model")
 @EnableErrorHandle
--- internal-payment/src/main/java/ru/tn/internalpayment/scheduled/ScheduledJob.java
+++ internal-payment/src/main/java/ru/tn/internalpayment/scheduled/ScheduledJob.java
@@ -1,5 +1,7 @@
 package ru.tn.internalpayment.scheduled;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,24 +11,24 @@ import ru.tn.model.Payment;
 
 import java.math.BigDecimal;
 
-/**
- * @author dsnimshchikov on 13.05.17.
- */
 @Component
 public class ScheduledJob {
+
     @Autowired
-    FeignController extPay;
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    private FeignController extPay;
 
     @Scheduled(fixedDelay = 3000)
+    @HystrixCommand(fallbackMethod = "defaultCallExternalPayment", commandProperties = {@HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE")})
     public void callExternalPayment() {
-
         Payment payment = new Payment();
         payment.setAccountNumber("00009999");
         payment.setClientName("Client FIO");
         payment.setTransferSum(BigDecimal.valueOf(1000));
 
         extPay.pay(payment);
+    }
+
+    public void defaultCallExternalPayment() {
+
     }
 }
